381593ecf9f5b5a6156bdad3485f5c437c27420f
Marx L. Canaza <denotefull@gmail.com>
2021-07-04 19:12:20 -0500

    User https solo en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(config('app.env') === 'production') {
+        if(App::environment('production')) {
             URL::forceScheme('https');
         }
     }
