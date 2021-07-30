cf1cb3115a7326274af9676b6a4fce81669b3be8
Marx L. Canaza <denotefull@gmail.com>
2021-07-30 18:43:30 -0500

    Agregar icono

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{env('APP_NAME')}}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
