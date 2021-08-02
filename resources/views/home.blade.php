404bc1b9f3f1c90100fdf9b2ad2e45e59a3c9532
Marx L. Canaza <denotefull@gmail.com>
2021-08-02 12:29:38 -0500

    Agregar banner login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
 
 @section('content')
     <div class="container pt-5">
+        @guest
+            @include('sections.banner_login')
+        @endguest
         <seccion-posts-component></seccion-posts-component>
     </div>
 
