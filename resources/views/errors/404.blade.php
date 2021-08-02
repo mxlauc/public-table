976516a9b2e57de6fbb7476da687f6636a490a38
Marx L. Canaza <denotefull@gmail.com>
2021-08-02 12:27:53 -0500

    Agregar traduccion a pagina de error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,15 +5,15 @@
     <div class="p-2 p-sm-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-3 py-md-0">
           <h1 class="display-5 fw-bold">
-            ¡Vaya!
+            {{__('Wow!')}}
             <br>
-            No ha sido posible encontrar esta página
+            {{__('This page could not be found')}}
           </h1>
           <p class="col-md-8 fs-4 my-4">
-            Esto puede deberse a que el propietario elimino el contenido o la dirección es incorrecta
+            {{__('This may be because the owner removed the content or the address is incorrect')}}
           </p>
           <a href="/" class="btn btn-primary">
-            Ir a la página de inicio
+            {{__('Go to homepage')}}
         </a>
         </div>
       </div>
